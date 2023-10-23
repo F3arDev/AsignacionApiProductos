2c441b003b0a24fa0a684ee67e392e4b8c9e56d8
fenderson <freddenderson@gmail.com>
2023-10-22 20:12:33 -0600

    db Cambios

--- SQL/Query InvProducto.sql
+++ SQL/Query InvProducto.sql
@@ -4,29 +4,43 @@ use invProducto;
 
 
 Create table producto(
-codProducto int primary key identity(1,1) not null,
-nombreProducto varchar(50),
-precioVenta float,
-costoUnitatio float,
-unidadesExistente int,
-cantidadMinima int,
-cantidadMaxima int,
-Estado varchar(25)
+	codProducto int primary key identity(1,1) not null,
+	nombreProducto varchar(50),
+	precioVenta float,
+	costoUnitatio float,
+	unidadesExistente int,
+	cantidadMinima int,
+	cantidadMaxima int,
+	Estado varchar(25)
 );
 
-Create table factura(
-numeroFactura int primary key identity(1,1),
-fecha date,
-detalleProducto int
 
-Foreign key (detalleProducto) references Producto (codProducto) 
-)
+-- Crear la tabla "Factura" con FLOAT
+CREATE TABLE factura (
+    codFactura INT PRIMARY KEY IDENTITY not null,
+    fecha DATE,
+    total FLOAT
+);
+
+-- Crear la tabla "DetalleFactura" con relaciones con FLOAT
+CREATE TABLE detalleProducto (
+    cod INT PRIMARY KEY IDENTITY not null,
+    codFactura INT, -- Esta columna se utiliza para relacionar con la tabla "Factura"
+    codProducto INT, -- Esta columna se utiliza para relacionar con la tabla "Producto"
+    nombreProducto NVARCHAR(50),
+	precio FLOAT,
+    cantidad INT,
+    subtotal FLOAT
+);
+
+-- Establecer la relación entre "DetalleFactura" y "Factura"
+ALTER TABLE detalleProducto
+ADD CONSTRAINT FK_detalleProducto_factura FOREIGN KEY (codFactura) REFERENCES factura(codFactura);
+
+-- Establecer la relación entre "DetalleFactura" y "Producto"
+ALTER TABLE detalleProducto
+ADD CONSTRAINT FK_detalleProducto_producto FOREIGN KEY (codProducto) REFERENCES producto(codProducto);
 
 insert into producto(nombreProducto, precioVenta, costoUnitatio, unidadesExistente, cantidadMinima, cantidadMaxima, Estado) values
 ('pan', 20, 15, 5, 10, 15, 'habilitado')
 select * from producto
-
-/*create table detalleProducto(
-NombreProducto varchar(50),
-
-)
